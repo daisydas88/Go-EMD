15c44e5c0ce07a98f99ddf19eb0f25f23600052d
daisydas88 <133199644+daisydas88@users.noreply.github.com>
2023-05-15 16:09:37 +0530

    Add files via upload

---
+++ spectralFeatures.m
@@ -0,0 +1,17 @@
+% For reconstructed spectral features
+% code for peak frequency
+fs=200;
+k= fft(s);
+
+f=(0:length(k)-1)*fs/length(k);
+plot(f,abs(k));
+pow= k.*conj(k);
+meanFreq = meanfreq(k);
+medianFreq= medfreq(k);
+totalPower = sum(pow);
+[maxAmp, maxAmpIdx] = max(abs(k));
+peakFreq = f(maxAmpIdx);
+disp(peakFreq);
+% code for no of peaks
+a= findpeaks(rs);
+disp(length(a));
